bde96df18dab82b8cf52a47032b647a43e337f1a
Atomico592 <atomico592@gmail.com>
2024-02-21 14:39:10 +0600

    Create and set up a project

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack"
+import {NavigationContainer, DefaultTheme} from "@react-navigation/native"
+import {useFonts} from "expo-font";
+import StackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: "transparent"
+    }
+}
+const App = () => {
+    return (
+        <NavigationContainer theme={theme}>
+            <StackNavigator screenOptions={{ headerShown : false }}
+            initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Details" component={Details}/>
+            </StackNavigator>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
