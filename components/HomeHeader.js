92e804cc1fa7f89aaac74ce583e96bd57f9abcc1
Atomico592 <atomico592@gmail.com>
2024-02-21 16:54:38 +0600

    Add card components, add content directory, add buttons

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View} from "react-native";
 
-const NftCard = () => {
+const HomeHeader = () => {
     return (
         <View>
-            
+
         </View>
     );
 };
 
-export default NftCard;
+export default HomeHeader;
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,44 @@
-import { View, SafeAreaView, FlatList, Text} from "react-native";
-import { COLORS, NFTData } from "../constants";
+import {FlatList, SafeAreaView, View} from "react-native";
+import {COLORS, NFTData} from "../constants";
+import {FocusedStatusBar, HomeHeader, NFTCard} from "../components";
 
 const Home = () => {
     return (
-       <SafeAreaView flex={{flex: 1}}>
+        <SafeAreaView style={{flex: 1}}>
+            <FocusedStatusBar background={COLORS.primary}/>
 
-       </SafeAreaView>
+            <View style={{flex: 1}}>
+                <View style={{zIndex: 0}}>
+                    <FlatList
+                        data={NFTData}
+                        renderItem={({item}) =>
+                            <NFTCard data={item}/>
+                        }
+                        keyExtractor={(item) => item.id}
+                        showsVerticalScrollIndicator={true}
+                        ListHeaderComponent={<HomeHeader/>}
+                    />
+                </View>
+                <View
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        bottom: 0,
+                        right: 0,
+                        left: 0,
+                        zIndex: -1,
+                    }}
+                >
+                    <View
+                        style={{height: 300, backgroundColor: COLORS.primary}}
+                    />
+                    <View
+                        style={{flex: 1, backgroundColor: COLORS.white}}
+                    />
+                </View>
+            </View>
+
+        </SafeAreaView>
     );
 };
 
